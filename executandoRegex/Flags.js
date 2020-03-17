ed23f60fb11d7fd2a79bf4e5d013f47b61bfe4a4
Roney-Mendes <roneymendes1@gmail.com>
2020-03-17 20:01:47 -0300

    first commit

---
+++ executandoRegex/Flags.js
@@ -0,0 +1,7 @@
+// g - global
+// i - ignore case
+
+const texto = 'Carlos assinou o abaixo-assinado.'
+console.log(texto.match(/C|ab/))
+console.log(texto.match(/C|ab/i))
+console.log(texto.match(/C|ab/gi))
